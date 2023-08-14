267072b01859befa1df93bb72fe6854574bd80b7
Johannes Pfister <johannes.pfister@josttech.ch>
2023-08-14 19:43:15 +0000

    Use a more neutral name for the generated brainfuck code

--- convert_complete.sh
+++ convert_complete.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#Script to generates the brainfuck code, so that there is a brainfuck interpreter written in 
+#Script to generates the brainfuck code, so that there is a brainfuck interpreter written in
 # brainfuck. And it compiles interpret.c to a native running interpreter
-#The output files are crazy.bf and crazy_minimal.bf
+#The output files are interpret.bf and interpret_minimal.bf
 
 set -eu
 
@@ -19,13 +19,14 @@ bash ./generate_simple.sh
 
 #Format the code. Not needed but can it make easier to debug the resulting brainfuck code in
 # case we use a brainfuck interpreter that can output debug information including line number
-astyle --style=gnu --indent=spaces=2 --add-brackets --indent-preproc-cond ./interpret_.c || true 
+astyle --style=gnu --indent=spaces=2 --add-brackets --indent-preproc-cond ./interpret_.c || true
 rm ./interpret_.c.orig || true
 
-python3 ./convert.py < ./interpret_.c > crazy.bf
-#create crazy_minimal.bf with only valid code characters. Note: tr doesn't accept every order but
-# '][+,.><-' seems to work
-tr -cd '][+,.><-'  < crazy.bf > crazy_minimal.bf
+python3 ./convert.py < ./interpret_.c > interpret.bf
+
+#create interpret_minimal.bf with only valid code characters. Note: tr doesn't accept every order
+# but '][+,.><-' seems to work
+tr -cd '][+,.><-'  < interpret.bf > interpret_minimal.bf
 
 bash ./compile.sh
 
