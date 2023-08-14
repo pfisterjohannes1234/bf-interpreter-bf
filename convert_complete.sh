d8cbd2f7f670e7ce222309cbe4682df3563f51f7
Johannes Pfister <johannes.pfister@josttech.ch>
2023-08-14 12:06:20 +0000

    Add script that generates the brainfuck file(s) from interpret.c
    This uses the already existing convert.py

---
+++ convert_complete.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -eu
+
+scriptError()
+{
+  echo "Error on line $1" 1>&2
+  exit 1
+}
+
+trap 'scriptError $LINENO' ERR
+
+
+bash ./generate_simple.sh
+
+#Format the code. Not needed but can it make easier to debug the resulting brainfuck code in
+# case we use a brainfuck interpreter that can output debug information including line number
+astyle --style=gnu --indent=spaces=2 --add-brackets --indent-preproc-cond ./interpret_.c || true 
+rm ./interpret_.c.orig || true
+
+python3 ./convert.py < ./interpret_.c > crazy.bf
+tr -cd "+-,.[]<>"  < crazy.bf > crazy_minimal.bf
+
+
